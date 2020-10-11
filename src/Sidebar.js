cb2e74047ae64efdf84231cd1930be91d4a5a6f0
Javier <ringo.larios@gmail.com>
2020-10-10 20:24:40 -0500

    adding the draft side bar

---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>I am the side bar</h2>
+
+      <div className="sidebar__top"></div>
+    </div>
+  );
+}
+
+export default Sidebar;
