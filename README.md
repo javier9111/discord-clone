83f4ef0df6b9b3b38b8f395cd2070d4eac146443
Javier <ringo.larios@gmail.com>
2020-10-11 19:03:24 -0500

    Adding a side bar with styles

--- README.md
+++ README.md
@@ -66,3 +66,7 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+### Web content for icons
+
+This wep app, uses Material icons, you can look for this in the followin link https://material-ui.com/components/material-icons/
--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import './App.css';
-import Sidebar from './Sidebar';
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     //BEM naming convention
     <div className="app">
-        <h1>Lets Build a redux app</h1>
-
-
-        {/*side bar */}
-        <Sidebar></Sidebar>
-        {/*chat */}
-
+      {/*side bar */}
+      <Sidebar></Sidebar>
+      {/*chat */}
     </div>
   );
 }
 
 export default App;
-
